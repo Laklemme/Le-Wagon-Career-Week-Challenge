45fce3c0089a683fb47a6e7d2243e04d10273448
Lara Klemme <laraklemme@gmail.com>
2021-09-09 20:29:28 +0200

    first try

---
+++ app/controllers/museums_controller.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'open-uri'
+
+class MuseumsController < ApplicationController
+  def index
+    if params[:query1].present? && params[:query2].present?
+      @museum_list = Museum.new
+      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{params[:query1]},#{params[:query2]}.json?types=poi&access_token=#{ENV['MAPBOX_API_KEY']}"
+      raw_data = URI.open(url).read
+      @museums = JSON.parse(raw_data)
+    else
+      @museum_list = Museum.new
+    end
+  end
+end
